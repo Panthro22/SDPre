0f12bbf234ad7adc9874e1ae5be3ed5eb073b0c9
Panthro22 <jordonpaynter22@gmail.com>
2021-09-10 00:52:14 -0600

    Completed Objective 3 Lab 2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2{
+
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+    side1 = 3;
+    side2 = 4;
+    hypotenuse = Math.sqrt(side1*side1 + side2*side2);
+    double num = Math.sqrt(3 * 3);    //num => 3
+    double result = Math.sqrt(4) + Math.sqrt(9); //result => 2 + 3 => 5
+    System.out.println("Hypotenuse: "+ hypotenuse);
+  }
+
+}
