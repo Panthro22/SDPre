4a60d27e03e3a3e91dff109f85027a1049500d4e
Panthro22 <jordonpaynter22@gmail.com>
2021-09-10 00:38:11 -0600

    Completed Objective 2 Lab 3
    git status
    clear

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 2.3;
+    num2 = 3.1;
+    num3 = 1.4;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println("average: "+average);
+  }
+}
