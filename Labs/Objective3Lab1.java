5585ffe9d52177b59c6217e20c3585d84218e502
Panthro22 <jordonpaynter22@gmail.com>
2021-09-10 00:45:01 -0600

    Complete Objective 3 Lab 1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 56.32;
+    num2 = 23.75;
+    num3 = 98.999;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println("Average: "+average); 
+  }
+}
