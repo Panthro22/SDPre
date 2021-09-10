2dd6c27248d3fba77e84c41ad04d49fd82baece5
Panthro22 <jordonpaynter22@gmail.com>
2021-09-10 00:24:59 -0600

    Completed Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jordon Paynter";
+    int age = 29;
+    String state = "Georgia";
+    System.out.println(name+"\n"+age+"\n"+state);
+  }
+}
