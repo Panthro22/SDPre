ead9c6deac09b3eca38e0827670dad1e1a3b6d97
Panthro22 <jordonpaynter22@gmail.com>
2021-09-11 00:56:29 -0600

    Completed Obejective 7 Lab 4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+
+  public static void main(String [] args){
+    int sumTotal = 0, count = 1;
+
+    while(count <= 20){
+      sumTotal = sumTotal + count;
+      count++;
+    }
+    System.out.println(sumTotal);
+  }
+}
